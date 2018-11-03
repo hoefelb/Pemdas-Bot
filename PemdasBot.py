2343fc686676019379289bc17221a57ee7d55c22
Bradley Hoefel <36757238+hoefelb@users.noreply.github.com>
2018-11-03 16:00:59 -0700

    Added credit to Tweepy
    Credited Tweepy, which was used to get and send tweets with Python

--- PemdasBot.py
+++ PemdasBot.py
@@ -1,6 +1,8 @@
 #A simple twitter bot that evaluates mathmatical expressions when it is mentioned in a tweet.
 #much thanks to pythonprogramming.net, where most of this code comes from
 #source: https://pythonprogramming.net/twitter-api-streaming-tweets-python-tutorial/
+#thanks to Tweepy for interfacing the code with Twitter's APIs
+#source: https://github.com/tweepy/tweepy
 #also thanks to r/python for finding a safe alternative to eval() for unsanitized data
 #source: https://www.reddit.com/r/Python/comments/3r7e31/alternatives_to_eval_which_is_said_to_be_never/
 
